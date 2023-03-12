60e957902e3b8e4e116f97e30476437b671fbc2e
zhangsscc99 <yw4554@nyu.edu>
2023-03-12 19:36:04 +0800

    update

--- src/main/java/com/nyu/controller/AuthorizeController.java
+++ src/main/java/com/nyu/controller/AuthorizeController.java
@@ -2,31 +2,45 @@ package com.nyu.controller;
 
 
 import com.nyu.dto.AccessTokenDTO;
+import com.nyu.dto.GithubUser;
 import com.nyu.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
 
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret")
+    private String clientSecret;
+    @Value("${github.redirect.url")
+    private String redirectUrl;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state
-    ){
+    ) throws IOException {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("0f85b8326afc18e6ff62");
-        accessTokenDTO.setClient_secret("f9ec70c095826467d195b5085f65c1e98ca9d869");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_url("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_url(redirectUrl);
         accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = GithubProvider.getUser(accessToken);
+        System.out.println(user.getName());
 
-        githubProvider.getAccessToken(accessTokenDTO);
+
         return "index";
     }
 }
--- src/main/java/com/nyu/provider/GithubProvider.java
+++ src/main/java/com/nyu/provider/GithubProvider.java
@@ -3,14 +3,23 @@ package com.nyu.provider;
 import com.alibaba.fastjson2.JSON;
 import com.nyu.dto.AccessTokenDTO;
 import com.nyu.dto.GithubUser;
+import com.sun.deploy.net.DownloadException;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 
 @Component
 public class GithubProvider {
-    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+//added codes
+
+
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) throws IOException {
 //mediatype.get or mediatype.parse
         MediaType mediaType = MediaType.parse("application/json; charset=utf-8");
 
@@ -23,18 +32,22 @@ public class GithubProvider {
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
                 .build();
-        try  (Response response = client.newCall(request).execute()){
+        try {
+            Response response = client.newCall(request).execute();
+
 
             String string = response.body().string();
             return response.body().string();
-        } catch (IOException e) {
+        } catch (Exception e) {
+            System.out.println("发生了异常：" + e.getMessage());
+
 
         }
         return null;
 
     }
 
-    public GithubUser getUser(String accessToken){
+    public static GithubUser getUser(String accessToken){
         OkHttpClient client = new OkHttpClient();
         //RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder()
@@ -44,9 +57,13 @@ public class GithubProvider {
         try {
             Response response = client.newCall(request).execute();
             String string = response.body().string();
+
+            String token = string.split("&")[0].split("=")[1];
             GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
             return githubUser;
-        } catch (IOException e) {
+        } catch (Exception e) {
+            System.out.println("发生了异常：" + e.getMessage());
+
 
         }
         return null;
