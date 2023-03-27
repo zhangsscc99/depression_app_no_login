e269b8beaea742e51497cf8227bfc9c32a269d6d
Yuting Wang <zhangsscc99@126.com>
2023-03-27 17:24:39 +0800

    update

--- src/main/java/com/nyu/controller/AuthorizeController.java
+++ src/main/java/com/nyu/controller/AuthorizeController.java
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -46,7 +48,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request) throws IOException {
+                           HttpServletRequest request,
+                           HttpServletResponse response) throws IOException {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -61,12 +64,14 @@ public class AuthorizeController {
 
         if (githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
+            response.addCookie(new Cookie("token", token));
             request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
         }else{
--- src/main/java/com/nyu/mapper/UserMapper.java
+++ src/main/java/com/nyu/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.nyu.mapper;
 import com.nyu.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 
@@ -12,4 +13,8 @@ public interface UserMapper {
 
     @Insert("insert into user (name, account_id, token, gmt_create, gmt_modified) values (#(name), #(accountId), #(token), #(gmtCreate), #(gmtModified)")
     void insert(User user);
+
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
